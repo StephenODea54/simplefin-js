c8265a604a51b3457c60e106471711d3b69ab859
StephenODea54 <odeastephen1@gmail.com>
2025-11-08 01:53:30 -0500

    add test for empty string for SimpleFin client

---
+++ src/client.test.ts
@@ -0,0 +1,8 @@
+import { SimpleFin } from "./client.js";
+import { describe, expect, it } from "vitest";
+
+describe("SimpleFinClient", () => {
+    it("should throw if setup token is empty", async () => {
+        await expect(SimpleFin.fromSetupToken("")).rejects.toThrow("Setup token is required to generate an access URL.");
+    });
+});
